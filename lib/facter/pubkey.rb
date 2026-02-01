da56b3474f1d23d07df0c09e751d7e957e6be108
Edd Miles <edw@rdmil.es>
2026-02-01 14:18:00 +0000

    Remove return path

--- lib/facter/pubkey.rb
+++ lib/facter/pubkey.rb
@@ -7,7 +7,6 @@ def pubkey_fetch_key(path)
   lines = IO.readlines(path, chomp: true)
   key = pubkey_parse_ssh_key(lines.join(''))
   cert = "#{path[0...-4]}-cert.pub"
-  return key unless File.file?(cert)
   expiry = Facter::Core::Execution.execute("ssh-keygen -L -f #{cert} 2>/dev/null | grep 'Valid:' | awk '{print $5}' | date -f - +%s ")
   key['expiry'] = expiry.empty? ? -1 : expiry.to_i
   return key
